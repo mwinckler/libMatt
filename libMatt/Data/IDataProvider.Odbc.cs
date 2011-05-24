ad0df268e2cb41a9f30b2b71b94ae5e95a0fd4b3
Matt Winckler <matt.winckler@gmail.com>
2011-05-24 14:29:10 -0700

    Added ODBC data provider

---
+++ libMatt/Data/IDataProvider.Odbc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Odbc;
+
+namespace libMatt.Data {
+	public class OdbcDataProvider: IDataProvider {
+
+		private string _conn_str;
+
+		public OdbcDataProvider(string connectionString) {
+			_conn_str = connectionString;
+		}
+
+		#region IDataProvider Members
+
+		public System.Data.IDbConnection CreateConnection() {
+			OdbcConnection conn = new OdbcConnection(_conn_str);
+			conn.Open();
+			return conn;
+		}
+
+		public System.Data.IDbDataAdapter CreateDataAdapter() {
+			return new OdbcDataAdapter();
+		}
+
+		#endregion
+	}
+}
